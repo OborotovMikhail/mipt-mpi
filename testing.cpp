efd7438f07e0f7fbb0821d7d91f758791d0930bb
OborotovMikhail <oborotoffm@yandex.ru>
2022-03-25 23:35:06 +0300

    fix

---
+++ testing.cpp
@@ -0,0 +1,18 @@
+#include <mpi.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv)
+{
+  printf("Before MPI_INIT\n");
+
+  MPI_Init(&argc, &argv);
+
+  printf("Parallel sect\n");
+
+  MPI_Finalize();
+
+  printf("After MPI_FINALIZE\n");
+  
+  return 0;
+}
