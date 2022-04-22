443fb8a566e4655290c4ad549a62c03590984e5e
OborotovMikhail <oborotoffm@yandex.ru>
2022-04-22 07:17:34 +0300

    added description

--- Lab1_TransportEquation/TransportEquation.c
+++ Lab1_TransportEquation/TransportEquation.c
@@ -2,9 +2,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/*Develop a parallel program using MPI technology that
+numerically solves the transfer equation according to a difference scheme in accordance with
+an individual variant*/
+
 int main(int argc, char **argv)
 {
-  
+
   
   return 0;
 }
--- Lab1_TransportEquation/TransportEquationNonMPI.c
+++ Lab1_TransportEquation/TransportEquationNonMPI.c
@@ -2,6 +2,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/*Develop a sequential program that numerically solves
+the transfer equation according to the difference scheme in accordance with the individual variant*/
+
 int main(int argc, char **argv)
 {
   
