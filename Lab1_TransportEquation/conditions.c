8fe7d0fa83f5ec0f430b5b50d55f6a6544ed4f6b
OborotovMikhail <thejarveyz@gmail.com>
2022-04-23 18:19:05 +0300

    added condition functions

---
+++ Lab1_TransportEquation/conditions.c
@@ -0,0 +1,9 @@
+#pragma once
+
+float conditionX(float x) {
+	return x;
+}
+
+float conditionT(float T); {
+	return T;
+}
---
+++ Lab1_TransportEquation/conditions.h
@@ -0,0 +1,9 @@
+#pragma once
+
+// Condition function for coodrinate x
+// Returns the value of the U(x, 0) function
+float conditionX(float x);
+
+// Condition function for time T
+// Returns the value of the U(0, t) function
+float conditionT(float T);
