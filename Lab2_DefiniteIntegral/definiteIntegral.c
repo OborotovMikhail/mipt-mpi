8c8483813307ada75df39b142ca79c222303efdb
OborotovMikhail <thejarveyz@gmail.com>
2022-05-02 20:46:12 +0300

    created lab 2 files

---
+++ Lab2_DefiniteIntegral/definiteIntegral.c
@@ -0,0 +1,12 @@
+/*Develop a parallel program using MPI technology that
+numerically solves the transfer equation according to a difference scheme in accordance with
+an individual variant*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv) {
+  
+
+  return 0;
+}
