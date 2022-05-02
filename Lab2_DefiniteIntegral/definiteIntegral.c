04e707b54fcad422ec16a7f0ae1405f8989ee2c6
OborotovMikhail <thejarveyz@gmail.com>
2022-05-03 01:35:04 +0300

    parameters txt file

--- Lab2_DefiniteIntegral/definiteIntegral.c
+++ Lab2_DefiniteIntegral/definiteIntegral.c
@@ -1,12 +1,28 @@
-/*Develop a parallel program using MPI technology that
-numerically solves the transfer equation according to a difference scheme in accordance with
-an individual variant*/
+/*With the help of pthread, it is necessary to make a program that will calculate 
+a definite integral of sin(1/x) function in the limit from a certain positive real 
+number to some larger positive real number. It is necessary to balance the 
+solution - the task execution time on all threads should be the same. 
+So, the integration steps must be dynamic*/
 
 #include <stdio.h>
 #include <stdlib.h>
 
+#define ERROR_OPEN_PARAMETERS 1
+
 int main(int argc, char **argv) {
-  
+  float xMin, xMax; // Definite integral limits (set as a parameter)
+  int totalIterations; // Total sum iterations (set as a parameter)
+
+  // Opening parameters file and reading parameters
+  FILE *fileParameters = fopen("parameters.txt", "r");
+  if (fileParameters == NULL) {
+    printf("Failed to open parameters file\n");
+    fclose(fileParameters);
+    exit(ERROR_OPEN_PARAMETERS);
+  } else {
+    fscanf(fileParameters, "%f %f %d", &xMin, &xMax, &totalIterations);
+    fclose(fileParameters);
+  }
 
   return 0;
 }
