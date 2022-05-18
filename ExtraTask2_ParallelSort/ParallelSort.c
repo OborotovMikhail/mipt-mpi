64b9f95808847fd2b5446f0085753fde3f261538
OborotovMikhail <oborotoffm@yandex.ru>
2022-05-18 11:17:22 +0300

    added parameters

--- ExtraTask2_ParallelSort/ParallelSort.c
+++ ExtraTask2_ParallelSort/ParallelSort.c
@@ -2,7 +2,20 @@
 #include <stdlib.h>
 
 int main(int argc, char **argv) {
-  
+  int arraySize, threadsNum; // Array size and the number of threads
+
+  // Opening parameters file and reading parameters
+  FILE *fileParameters = fopen("parameters.txt", "r");
+  if (fileParameters == NULL) {
+    printf("Failed to open parameters file\n");
+    fclose(fileParameters);
+    exit(ERROR_OPEN_PARAMETERS);
+  } else {
+    fscanf(fileParameters, "%d %d", &arraySize, &threadsNum);
+    fclose(fileParameters);
+  }
+
+  // Creating an array
 
   return 0;
 }
